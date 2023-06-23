feaeebf987079c542a9a612605ceba23e535ac85
Milos Pivas <pivasmilos@microsoft.com>
2023-06-23 23:13:37 +0200

    feat: add Decorator pattern

---
+++ src/Decorator/ModemDecorator.test.ts
@@ -0,0 +1,67 @@
+import { setup, teardown } from "../TestUtils/testUtils";
+import { ModemControlProgram, User, createModem } from "./ModemDecorator";
+
+describe("Decorator pattern", () => {
+  beforeEach(setup);
+  afterEach(teardown);
+
+  describe("ModemControlProgram", () => {
+    it("should dial a number using a Hayes modem", () => {
+      const modemControlProgram = new ModemControlProgram("Hayes");
+      const user = new User("Alice", "high");
+
+      user.dial("555-1234", modemControlProgram);
+
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-1234 loudly...");
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-1234 using Hayes modem...");
+    });
+
+    it("should dial a number using a USR modem", () => {
+      const modemControlProgram = new ModemControlProgram("USR");
+      const user = new User("Bob", "low");
+
+      user.dial("555-5678", modemControlProgram);
+
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-5678 quietly...");
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-5678 using USR modem...");
+    });
+  });
+
+  describe("ModemFactory", () => {
+    it("should create a loud dial Hayes modem", () => {
+      const modem = createModem("high", "Hayes");
+      modem.dial("555-1234");
+
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-1234 loudly...");
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-1234 using Hayes modem...");
+    });
+
+    it("should create a quiet dial USR modem", () => {
+      const modem = createModem("low", "USR");
+      modem.dial("555-5678");
+
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-5678 quietly...");
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-5678 using USR modem...");
+    });
+
+    it("should default to a loud dial Hayes modem", () => {
+      const modem = createModem("high");
+      modem.dial("555-1234");
+
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-1234 loudly...");
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-1234 using Hayes modem...");
+    });
+  });
+
+  describe("User", () => {
+    it("should dial a number using a modem", () => {
+      const modemControlProgram = new ModemControlProgram("Hayes");
+      const user = new User("Dave", "high");
+
+      user.dial("555-3456", modemControlProgram);
+
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-3456 loudly...");
+      expect(console.log).toHaveBeenCalledWith("Dialing 555-3456 using Hayes modem...");
+    });
+  });
+});
---
+++ src/Decorator/ModemDecorator.ts
@@ -0,0 +1,93 @@
+export class Modem {
+  dial(number: string): void {
+    console.log(`Dialing ${number}...`);
+  }
+
+  setSpeakerVolume(volume: "low" | "high"): void {
+    console.log(`Setting speaker volume to ${volume}...`);
+  }
+}
+
+export class HayesModem extends Modem {
+  override dial(number: string): void {
+    console.log(`Dialing ${number} using Hayes modem...`);
+  }
+}
+
+export class USRModem extends Modem {
+  override dial(number: string): void {
+    console.log(`Dialing ${number} using USR modem...`);
+  }
+}
+
+export type ModemTypes = "Hayes" | "USR";
+
+export class ModemControlProgram {
+  private readonly users: Record<string, { user: User; modem: Modem }> = {};
+
+  constructor(private modemType: ModemTypes) {}
+
+  public getModemType(): ModemTypes {
+    return this.modemType;
+  }
+
+  addUser(user: User, modem: Modem): void {
+    this.users[user.name] = { user, modem };
+  }
+
+  dial(number: string, userName: string): void {
+    const userModem = this.users[userName];
+
+    if (!userModem) {
+      console.log(`User ${userName} is not registered in the modem control program!`);
+      return;
+    }
+
+    console.log(`Dialing ${number} on behalf of ${userName}...`);
+    userModem.modem.dial(number);
+  }
+}
+
+export class LoudDialModem extends Modem {
+  constructor(private readonly modem: Modem) {
+    super();
+  }
+
+  override dial(number: string): void {
+    console.log(`Dialing ${number} loudly...`);
+    this.modem.dial(number);
+  }
+}
+
+export class QuietDialModem extends Modem {
+  constructor(private readonly modem: Modem) {
+    super();
+  }
+
+  override dial(number: string): void {
+    console.log(`Dialing ${number} quietly...`);
+    this.modem.dial(number);
+  }
+}
+
+/**
+ * Modem factory
+ */
+export function createModem(speakerVolume: "low" | "high", modemType: ModemTypes = "Hayes"): Modem {
+  const baseModem = modemType === "Hayes" ? new HayesModem() : new USRModem();
+  if (speakerVolume === "low") {
+    return new QuietDialModem(baseModem);
+  } else {
+    return new LoudDialModem(baseModem);
+  }
+}
+
+export class User {
+  constructor(public readonly name: string, public readonly speakerVolume: "low" | "high") {}
+
+  dial(number: string, modemControlProgram: ModemControlProgram): void {
+    const modem = createModem(this.speakerVolume, modemControlProgram.getModemType());
+    modemControlProgram.addUser(this, modem);
+    modemControlProgram.dial(number, this.name);
+  }
+}
