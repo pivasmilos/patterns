7226bfb37948d7fc928470d3637272afb3e939f5
Milos Pivas <pivasmilos@microsoft.com>
2023-06-24 23:14:56 +0200

    feat: add Extension Object

---
+++ src/ExtensionObject/PartExtension.test.ts
@@ -0,0 +1,88 @@
+import {
+  Assembly,
+  PiecePart,
+  createPartWithExtension,
+  PiecePartExplosionExtension,
+  AssemblyExplosionExtension,
+  PiecePartCromulentExtension,
+  AssemblyCromulentExtension,
+} from "./PartExtension";
+
+describe("PartExtension extension objects", () => {
+  describe("createPartWithExtension", () => {
+    it("should create a PiecePart with a PiecePartExplosionExtension when extensionType is 'explosion'", () => {
+      const piecePart = createPartWithExtension("piece", "Bolt", 0.5, "explosion");
+
+      const piecePartExplosionExtension = piecePart.partExtensionsMap["explosion"];
+
+      expect(piecePart).toBeInstanceOf(PiecePart);
+      expect(piecePartExplosionExtension?.extensionType).toBe("explosion");
+      expect(piecePartExplosionExtension).toBeInstanceOf(PiecePartExplosionExtension);
+      if (!(piecePartExplosionExtension instanceof PiecePartExplosionExtension)) {
+        // always true if the previous expect passed, but TypeScript doesn't know that
+        return;
+      }
+      expect(piecePartExplosionExtension.createExplosionReport()).toEqual(
+        "Piece explosion report: Bolt. For part piece at $0.5"
+      );
+    });
+
+    it("should create an Assembly with an AssemblyExplosionExtension when extensionType is 'explosion'", () => {
+      const part1 = createPartWithExtension("piece", "Bolt", 0.5, "explosion");
+      const part2 = createPartWithExtension("piece", "Nut", 0.3, "explosion");
+      const assembly = createPartWithExtension("assembly", "Bolt and Nut Assembly", 0.8, "explosion") as Assembly;
+      assembly.parts = [part1, part2];
+
+      const assemblyExplosionExtension = assembly.partExtensionsMap["explosion"];
+
+      expect(assembly).toBeInstanceOf(Assembly);
+      expect(assemblyExplosionExtension).toBeInstanceOf(AssemblyExplosionExtension);
+      if (!(assemblyExplosionExtension instanceof AssemblyExplosionExtension)) {
+        // always true if the previous expect passed, but TypeScript doesn't know that
+        return;
+      }
+      expect(assemblyExplosionExtension.createExplosionReport()).toEqual(
+        "Explosion report for assembly: Bolt and Nut Assembly.\nPiece explosion report: Bolt. For part piece at $0.5\nPiece explosion report: Nut. For part piece at $0.3"
+      );
+    });
+
+    it("should create a PiecePart with a PiecePartCromulentExtension when extensionType is 'cromulent'", () => {
+      const piecePart = createPartWithExtension("piece", "Bolt", 0.5, "cromulent");
+
+      const piecePartCromulentExtension = piecePart.partExtensionsMap["cromulent"];
+
+      expect(piecePart).toBeInstanceOf(PiecePart);
+      expect(piecePartCromulentExtension).toBeInstanceOf(PiecePartCromulentExtension);
+      if (!(piecePartCromulentExtension instanceof PiecePartCromulentExtension)) {
+        // always true if the previous expect passed, but TypeScript doesn't know that
+        return;
+      }
+      expect(piecePartCromulentExtension.cromulentize()).toEqual("Cromulent piece named Bolt");
+    });
+
+    it("should create an Assembly with an AssemblyCromulentExtension when extensionType is 'cromulent'", () => {
+      const part1 = createPartWithExtension("piece", "Bolt", 0.5, "cromulent");
+      const part2 = createPartWithExtension("piece", "Nut", 0.3, "cromulent");
+      const assembly = createPartWithExtension("assembly", "Bolt and Nut Assembly", 0.8, "cromulent") as Assembly;
+      assembly.parts = [part1, part2];
+
+      const assemblyCromulentExtension = assembly.partExtensionsMap["cromulent"];
+
+      expect(assembly).toBeInstanceOf(Assembly);
+      expect(assemblyCromulentExtension).toBeInstanceOf(AssemblyCromulentExtension);
+
+      if (!(assemblyCromulentExtension instanceof AssemblyCromulentExtension)) {
+        // always true if the previous expect passed, but TypeScript doesn't know that
+        return;
+      }
+
+      expect(assemblyCromulentExtension.cromulentize()).toEqual(
+        "Cromulent assembly: Bolt and Nut Assembly.\nCromulent piece named Bolt\nCromulent piece named Nut"
+      );
+    });
+
+    it("should throw an error for an unknown extension type", () => {
+      expect(() => createPartWithExtension("piece", "Bolt", 0.5, "unknown")).toThrow("Unknown extension type");
+    });
+  });
+});
---
+++ src/ExtensionObject/PartExtension.ts
@@ -0,0 +1,157 @@
+export interface ProductCatalog {
+  products: Product[];
+}
+
+export interface Product {
+  parts: Part[];
+}
+
+export interface PartExtension {
+  extensionType: string;
+}
+
+export interface Part {
+  name: string;
+  price: number;
+  partExtensionsMap: Record<string, PartExtension>;
+}
+
+export class PiecePart implements Part {
+  constructor(
+    public pieceType: string,
+    public name: string,
+    public price: number,
+    public partExtensionsMap: Record<string, PartExtension>
+  ) {}
+}
+
+export class Assembly implements Part {
+  constructor(
+    public parts: Part[],
+    public name: string,
+    public price: number,
+    public partExtensionsMap: Record<string, PartExtension>
+  ) {}
+}
+
+export interface PartExplosionExtension extends PartExtension {
+  createExplosionReport(): string;
+}
+
+export class PiecePartExplosionExtension implements PartExplosionExtension {
+  public extensionType = "explosion";
+
+  constructor(private piecePart: PiecePart) {}
+
+  createExplosionReport(): string {
+    return `Piece explosion report: ${this.piecePart.name}. For part ${this.piecePart.pieceType} at $${this.piecePart.price}`;
+  }
+}
+
+export class AssemblyExplosionExtension implements PartExplosionExtension {
+  public extensionType = "explosion";
+
+  constructor(private assembly: Assembly) {}
+
+  createExplosionReport(): string {
+    return `Explosion report for assembly: ${this.assembly.name}.\n${this.getPartsExplosion()}`;
+  }
+
+  private getPartsExplosion(): string {
+    return this.assembly.parts
+      .map((p) => {
+        const explosionExtension = p.partExtensionsMap["explosion"];
+
+        if (!explosionExtension) {
+          throw new Error("Part does not have an explosion extension");
+        }
+
+        if (
+          !(explosionExtension instanceof PiecePartExplosionExtension) &&
+          !(explosionExtension instanceof AssemblyExplosionExtension)
+        ) {
+          throw new Error("Part extension is not an explosion extension");
+        }
+
+        return explosionExtension.createExplosionReport();
+      })
+      .join("\n");
+  }
+}
+
+export interface PartCromulentExtension extends PartExtension {
+  cromulentize(): string;
+}
+
+export class PiecePartCromulentExtension implements PartCromulentExtension {
+  public extensionType = "cromulent";
+  constructor(private piecePart: PiecePart) {}
+
+  cromulentize(): string {
+    return `Cromulent piece named ${this.piecePart.name}`;
+  }
+}
+
+export class AssemblyCromulentExtension implements PartCromulentExtension {
+  public extensionType = "cromulent";
+  constructor(private assembly: Assembly) {}
+
+  cromulentize(): string {
+    return `Cromulent assembly: ${this.assembly.name}.\n${this.getPartsCromulentized()}`;
+  }
+
+  private getPartsCromulentized(): string {
+    return this.assembly.parts
+      .map((p) => {
+        const explosionExtension = p.partExtensionsMap["cromulent"];
+
+        if (!explosionExtension) {
+          throw new Error("Part does not have a cromulent extension");
+        }
+
+        if (
+          !(explosionExtension instanceof PiecePartCromulentExtension) &&
+          !(explosionExtension instanceof AssemblyCromulentExtension)
+        ) {
+          throw new Error("Part extension is not a cromulent extension");
+        }
+
+        return explosionExtension.cromulentize();
+      })
+      .join("\n");
+  }
+}
+
+// essentially a PartExtension factory
+export function createPartExtension(
+  part: Part,
+  extensionType: string
+): PartExplosionExtension | PartCromulentExtension {
+  if (extensionType === "explosion") {
+    if (part instanceof PiecePart) {
+      return new PiecePartExplosionExtension(part);
+    } else if (part instanceof Assembly) {
+      return new AssemblyExplosionExtension(part);
+    }
+  } else if (extensionType === "cromulent") {
+    if (part instanceof PiecePart) {
+      return new PiecePartCromulentExtension(part);
+    } else if (part instanceof Assembly) {
+      return new AssemblyCromulentExtension(part);
+    }
+  }
+
+  throw new Error("Unknown extension type");
+}
+
+// essentially a Part factory
+export function createPartWithExtension(
+  partType: "piece" | "assembly",
+  name: string,
+  price: number,
+  extensionType: string
+): Part {
+  const part = partType === "piece" ? new PiecePart(partType, name, price, {}) : new Assembly([], name, price, {});
+  part.partExtensionsMap[extensionType] = createPartExtension(part, extensionType);
+  return part;
+}
